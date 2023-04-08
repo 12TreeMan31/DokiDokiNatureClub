37f35324e2320a239a22ffba1e53a76fd35dd0d3
A.M <77717364+Bee-B0@users.noreply.github.com>
2023-04-08 10:44:43 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 import pygame
 import sys
 
-
 pygame.init()
+font = pygame.font.SysFont("georgia", 50)
 
 # create game window 
 
@@ -22,10 +22,82 @@ def drawText(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img,(x, y))
 
+
+objects = []
+
+class Button():
+    def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.onclickFunction = onclickFunction
+        self.onePress = onePress
+        self.alreadyPressed = False
+
+        self.fillColors = {
+            'normal': '#ffffff',
+            'hover': '#666666',
+            'pressed': '#333333',
+        }
+
+
+        self.buttonSurface = pygame.Surface((self.width, self.height))
+        self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+        self.buttonSurf = font.render(buttonText, True, (20, 20, 20))
+        
+        objects.append(self)
+
+    def process(self):
+        mousePos = pygame.mouse.get_pos()
+        self.buttonSurface.fill(self.fillColors['normal'])
+        if self.buttonRect.collidepoint(mousePos):
+            self.buttonSurface.fill(self.fillColors['hover'])
+            if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                self.buttonSurface.fill(self.fillColors['pressed'])
+                if self.onePress:
+                    self.onclickFunction()
+                elif not self.alreadyPressed:
+                    self.onclickFunction()
+                    self.alreadyPressed = True
+            else:
+                self.alreadyPressed = False
+
+        self.buttonSurface.blit(self.buttonSurf, [
+            self.buttonRect.width/2 - self.buttonSurf.get_rect().width/2,
+            self.buttonRect.height/2 - self.buttonSurf.get_rect().height/2
+        ])
+        screen.blit(self.buttonSurface, self.buttonRect)
+
+
+def buttonPressed():
+    print('Start Pressed')
+
+    objects.clear()
+
+
+
+
+
+
+
+
+
+
+def quitPressed():
+    print('Quit Pressed')
+    pygame.QUIT()
+
+Button(30, 200, 400, 100, 'start', buttonPressed)
+Button(30, 400, 400, 100, 'quit', quitPressed)
+
+
 run = True 
 while run:
     
     screen.fill((255, 209, 220))
+    
 
     drawText("Doki Doki Nature Club!!", pygame.font.SysFont("georgia", 70), TEXT_COL, 20 , 60)
 
@@ -36,6 +108,15 @@ while run:
         
         if event.type == pygame.QUIT:
             run = False
+            pygame.display.update()
+
+    for object in objects:
+        object.process()
+
+    pygame.display.flip()
+
         
-        pygame.display.update()
+
+    fpsClock.tick(fps)
+
 pygame.quit()
