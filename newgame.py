694a17e587cc949c188d675a3df5436f011ffcca
mwatermelon <m.watermelon20@gmail.com>
2023-04-08 16:02:52 -0500

    fix

--- newgame.py
+++ newgame.py
@@ -4,11 +4,17 @@ import pygame_menu
 pygame.init()
 window = pygame.display.set_mode((1280,720))
 
+pink = pygame_menu.themes.THEME_BLUE.copy()
+pink.title_background_color=(245, 103, 176)
+pink.title_font = pygame_menu.font.FONT_NEVIS
+
+
+
 
 def start():
     pass
 
-menu = pygame_menu.Menu('Doki Doki Nature Club', 1280, 720, theme=pygame_menu.themes.THEME_BLUE)
+menu = pygame_menu.Menu('Doki Doki Nature Club', 1280, 720, theme=pink)
 menu.add.button("Play", start)
 menu.add.button("Quit", pygame_menu.events.EXIT)
 
