48302af11e345a81ed44f90966fbf0292e8db882
A.M <77717364+Bee-B0@users.noreply.github.com>
2023-04-08 18:19:12 -0500

    Create runner.py

---
+++ runner.py
@@ -0,0 +1,8 @@
+import subprocess
+ 
+# Run the other script
+subprocess.run(["python", "a.py"])
+subprocess.run(["python","taiko.py"])
+subprocess.run(["python","b.py"])
+subprocess.run(["python","ddr.py"])
+subprocess.run(["python","c.py"]) 
